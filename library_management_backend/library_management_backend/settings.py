8308511d73838ab4715d35ef94ebef79f1163423
mountassisi-library <mountassisi.library@gmail.com>
2026-03-21 23:33:56 +0530

    settings recovered

--- library_management_backend/library_management_backend/settings.py
+++ library_management_backend/library_management_backend/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+# # #
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-^exqbk*o+yn)p-9!$*fzlh4a$ldm$$j14hx($6dakj7+l981k^'
 
